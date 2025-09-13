4e95209a6cc2d7a5268f377bb6a3f9d29a7da56f
Nevdas <prince.nullxd@gmail.com>
2025-09-12 20:15:02 -0400

    Update

--- Chapters/Chapter02.c
+++ Chapters/Chapter02.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
-#define KILOMETERS_TO_MILES 1.609
 
-int main(void){
-    double miles, kilometers;
+void CH2p2_variable_declarations_and_datatypes() {
+    double miles = 10;
+    double kms;
+    int amount;
+    char key;
+    char first_letter;
+    double a, b, c;
+}
 
-    printf("Enter the distance in miles.");
+void CH2p3_printf_function() {
+    int number = 50;
+    int miles;
+    double miles_to_kilometers;
+    
+    printf("This is my number: %d\n", number);
+
+    printf("Please enter the distance in miles: ");
+    fflush(stdout);
     scanf("%lf", &miles);
 
-    kilometers = KILOMETERS_TO_MILES * miles;
+    miles_to_kilometers = miles * 1.6;
+    printf("Miles to Kilometers-> %f\n", miles_to_kilometers);
+}
 
-    printf("That equals to");
-    printf("%f", kilometers);
+void CH2p3_scanning() {
+    char letter_1, letter_2, letter_3;
 
-    return 0;
+    scanf("%c%c%c", &letter_1, &letter_2, &letter_3);
+    printf("%c\n%c\n%c\n", letter_1, letter_2, letter_3);
+}
+
+int main(void){
+    CH2p3_scanning();
+    return (0);
 }
